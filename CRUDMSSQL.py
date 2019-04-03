18570c89af04a402615943ad3d5597d79ad2b056
Vermiis <Nira@Vermiis>
2019-04-03 22:49:09 +0200

    mssql

--- CRUDMSSQL.py
+++ CRUDMSSQL.py
@@ -6,25 +6,29 @@ config.read('config.ini')
 hostname = config['DEFAULT']['ServerName']
 
 
-#przyklad z neta, trzeba rozbic na funckje  :)
 
-conn = pymssql.connect(host=hostname, user='usrnm', password='passwd', Database='mydb')
-cur = conn.cursor()
+class CRUDMSSQL:
+      def __init__(self):
+            self.connection = ""
+            self.cursor1 = ""
 
-#CREATE , INSERT , UPDATE, SELECT always use execute command
-cur.execute('CREATE TABLE test(id INT, name VARCHAR(100))')
-cur.execute("INSERT INTO test(id,name) VALUES('3','mahendra')")
-cur.execute("UPDATE test set name='rony' where id='1'")
-cur.execute("DELETE from test where id='1'")
-conn.commit() # don't forget to commit after manipulating database
 
-#To retrieve data / select you can do this
-cur.execute('SELECT * from test')
-row = cur.fetchone()
+      def ExecuteQuery(self, query):
+            cur = self.cursor1()
+            cur.execute(query)
+            self.connection.commit
+            self.connection.close()
 
-while row:
+      def setConnection(self, hostname, usrname, pwd, dbname):
+            self.connection = pymssql.connect(host=hostname, user='usrnm', password='passwd', Database='mydb')
+
+      def ExecuteSelect(self, selectq):
+            cur = self.cursor1()
+            cur.execute(selectq)
+            row = cur.fetchone()
+            while row:
       #Index column fields in database always from 0
-      print (row[0],row[1])
-      row = cur.fetchone()
+                   print (row[0],row[1])
+                   row = cur.fetchone()
+            self.connection.close()
 
-conn.close() #don't forget close connection after all process CRUD complete
